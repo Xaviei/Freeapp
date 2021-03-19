90e168eac61b0013f8f5ab431a1f9d122a785114
Xavier <Xavie332@gmail.com>
2021-03-19 16:15:12 -0500

    added movement via kivy_clock time interval and introduced a lot basic movement for all characters

--- kev.py
+++ kev.py
@@ -24,36 +24,36 @@ class GameWidget(Widget):
     
     def _on_keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_key_down)
-        self._keyboard.unbind(on_key_up=self._on_key_up
+        self._keyboard.unbind(on_key_up=self._on_key_up)
         self._keyboard = None
 
     def _on_key_down(self,keyboard,keycode,text,modifiers):
-
         self.keysPressed.add(text)
-
-        currentx = self.player.pos[0]
-        currenty = self.player.pos[1]
-        
-        if text == 'w':
-            currenty += 1
-
-        if text == 's':
-            currenty -= 1
-        
-        if text == 'a':
-            currentx -= 1
-
-        if text == 'd':
-            currentx += 1
-        
-        
-        self.player.pos = (currentx,currenty)
 
     def _on_key_up(self,keyboard,keycode):
         text = keycode[1]
         if text in self.keysPressed:
             self.keysPressed.remove(text)
 
+    def move_step(self,dt):
+        currentx = self.player.pos[0]
+        currenty = self.player.pos[1]
+
+        step_size = 60 * dt
+
+        if 'w' in self.keysPressed:
+            currenty += step_size
+        
+        if 's' in self.keysPressed:
+            currenty -= step_size
+        
+        if 'a' in self.keysPressed:
+            currentx -= step_size
+
+        if 'd' in self.keysPressed:
+            currentx += step_size
+
+        self.player.pos = (currentx, currenty)
 
 class MyApp(App):
     def build(self):
